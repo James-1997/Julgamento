a2402eef7ce7fe95b258202061bfa6bed64bf10a
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-02-19 11:03:53 -0400

    Adicionando model dos personagens

---
+++ Julgamento/Model/ArrayPerfilDoPersonagem.swift
@@ -0,0 +1,29 @@
+//
+//  ArrayPerfilDoPersonagem.swift
+//  Julgamento
+//
+//  Created by Eliza Maria Porto de Carvalho on 19/02/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var arrayPerfilDoPersonagem: [PerfilDoPersonagem] = [
+    
+    
+    
+    PerfilDoPersonagem(nome: "Unicornio", personalidade: "Ainda vou escrever", papel: "Réu", image: <#T##UIImage#>),
+    
+    PerfilDoPersonagem(nome: "Elfo", personalidade: "Ainda vou escrever", papel: "Advogado de Dfesa", image: <#T##UIImage#>),
+    
+    PerfilDoPersonagem(nome: "Gnomo", personalidade: "Ainda vou escrever", papel: "Juri", image: <#T##UIImage#>),
+    
+    PerfilDoPersonagem(nome: "Bruxa", personalidade: "Ainda vou escrever", papel: "Promotora", image: <#T##UIImage#>),
+    
+    PerfilDoPersonagem(nome: "Diabrete", personalidade: "Ainda vou escrever", papel: "testemunha", image: <#T##UIImage#>),
+
+    
+    
+]
+
