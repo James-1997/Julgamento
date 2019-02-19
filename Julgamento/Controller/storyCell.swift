7b2adc2ebe8ff5e9e3ff88d548a51fa67d1b2d88
Robson James <james.robson.junior@gmail.com>
2019-02-19 11:19:54 -0400

    xib da cell da story

---
+++ Julgamento/Controller/storyCell.swift
@@ -0,0 +1,14 @@
+//
+//  personagemCell.swift
+//  Julgamento
+//
+//  Created by Robson James Junior on 19/02/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import UIKit
+
+class storyCell: UICollectionViewCell {
+    
+    
+}
