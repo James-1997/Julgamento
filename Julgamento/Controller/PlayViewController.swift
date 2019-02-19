57d0e0965627c618f08183dfd05039157e3a79a4
Robson James <james.robson.junior@gmail.com>
2019-02-19 09:45:59 -0400

    Controller da PlayView e das RegrasView

---
+++ Julgamento/Controller/PlayViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Julgamento
+//
+//  Created by Robson James Junior on 19/02/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+    
+    
+    // MARK: viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
