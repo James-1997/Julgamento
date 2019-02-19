339e7ee4f53c2d85ef84cd8cb037841e4b2bd3ab
Robson James <james.robson.junior@gmail.com>
2019-02-19 12:03:35 -0400

    teste collection View

--- Julgamento/Model/ArrayDeEstorias.swift
+++ Julgamento/Model/ArrayDeEstorias.swift
@@ -9,9 +9,9 @@
 import Foundation
 import UIKit
 
-var arrayDeEstórias: [Story] = [
-    
-    Story(titulo: "O tráfico de Pó Mágico", história: "A duas noites atrás, foi encontrado um carregamento ilícito de pó mágico no lago dos duendes. No local do crime foram encontrados algumas pegadas de um animal quadrúpede. Cid o unicórnio que mora nas redondezas está sendo julgado aqui, hoje, como o principal suspeito. ", gênero: "fantasia", image: <#T##UIImage#>),
-    
-    
-]
+//var arrayDeEstórias: [Story] = [
+//    
+//    Story(titulo: "O tráfico de Pó Mágico", história: "A duas noites atrás, foi encontrado um carregamento ilícito de pó mágico no lago dos duendes. No local do crime foram encontrados algumas pegadas de um animal quadrúpede. Cid o unicórnio que mora nas redondezas está sendo julgado aqui, hoje, como o principal suspeito. ", gênero: "fantasia", image: <#T##UIImage#>),
+//    
+//    
+//]
