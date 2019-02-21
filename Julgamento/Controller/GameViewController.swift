d6f13af856ed699eb49e66c487a4ea76ece98709
Lucídio Souza <lucidio.abs@gmail.com>
2019-02-21 11:11:34 -0400

    add timer GAMEVIEW

--- Julgamento/Controller/GameViewController.swift
+++ Julgamento/Controller/GameViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class GameViewController: UIViewController {
     
@@ -31,9 +32,52 @@ class GameViewController: UIViewController {
     
     @IBOutlet weak var backGroudImage: UIImageView!
     
+    @IBOutlet weak var timerLabel: UILabel!
     
-    var históriaSelecionada: Story = arrayDeEstórias[0]
+     var históriaSelecionada: Story = arrayDeEstórias[0]
     
+    
+    var time = 10
+    
+    var timer = Timer()
+    @IBAction func start(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameViewController.action), userInfo: nil, repeats: true)
+    }
+    
+    // funcao usada p/ dizer o que vai acontecer durante o decorrer do timer
+    @objc func action (){
+        
+        let min =  time/60
+        let seg = time%60
+        
+        if time >= 70{
+            timerLabel.text = "\(String(min)):\(String(seg))"
+            time -= 1
+        }
+            
+        else if time > 60{
+            timerLabel.text = "\(String(min)):0\(String(seg))"
+            time -= 1
+        }
+            
+        else if time == 60{
+            timerLabel.text = "0:00"
+            time -= 1
+        }
+        else if time < 60 && time >= 10{
+            timerLabel.text = "0:\(String(seg))"
+            // timerLabel.text = String(time)
+            time -= 1
+        }
+        else if time < 10{
+            timerLabel.text = "0:0\(String(seg))"
+            time -= 1
+        }
+        if time < 0{
+            timer.invalidate()
+        }
+        
+    }
     // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
--- Julgamento/Model/ArrayPerfilDoPersonagem.swift
+++ Julgamento/Model/ArrayPerfilDoPersonagem.swift
@@ -18,9 +18,9 @@ var arrayPerfilDoPersonagem: [PerfilDoPersonagem] = [
     
     PerfilDoPersonagem(nome: "Gnomo", personalidade: "Gnomos são muito astutos e inteligentes. Tendem a ter e proteger bens preciosos. Alguns são muito bons e gentis embora outros sejam gananciosos e desonestos. ", papel: "Juri", image: #imageLiteral(resourceName: "image1")),
     
-    PerfilDoPersonagem(nome: "Bruxa", personalidade: "Bruxas são grandes conhecedoras da natureza e de feitiços mágicos. Algumas costumam utilizar seus poderes e sabedoria para cura, embora outras utilizem para a prática de mágia negra. ", papel: "Promotora", image: <#T##UIImage#>),
-
-    PerfilDoPersonagem(nome: "Diabrete", personalidade: "Diabretes são muito travessos e ajudam as Bruxas e o Senhor das Trevas. São muito pequenos apesar de que alguns dizem que eles podem ter o tamanho que quiserem.", papel: "testemunha", image: <#T##UIImage#>),
+//    PerfilDoPersonagem(nome: "Bruxa", personalidade: "Bruxas são grandes conhecedoras da natureza e de feitiços mágicos. Algumas costumam utilizar seus poderes e sabedoria para cura, embora outras utilizem para a prática de mágia negra. ", papel: "Promotora", image: <#T##UIImage#>),
+//
+//    PerfilDoPersonagem(nome: "Diabrete", personalidade: "Diabretes são muito travessos e ajudam as Bruxas e o Senhor das Trevas. São muito pequenos apesar de que alguns dizem que eles podem ter o tamanho que quiserem.", papel: "testemunha", image: <#T##UIImage#>),
 
     
 ]
