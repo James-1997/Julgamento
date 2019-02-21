943e9ab1ecf06791f701c170dedd0b8acaaab65b
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-02-21 13:58:34 -0400

    Adicionando Pop Up "End of Turn"

---
+++ Julgamento/Controller/PopUps/EndPopUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EndPopUpViewController.swift
+//  Julgamento
+//
+//  Created by Eliza Maria Porto de Carvalho on 21/02/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import UIKit
+
+class EndPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+   
+}
