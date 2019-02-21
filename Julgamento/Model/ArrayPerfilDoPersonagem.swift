8267d91c8bc5202b2cb98cd73235f5107ee5d091
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-02-21 10:58:05 -0400

    update ArrayPerfilDosPersonagens

--- Julgamento/Model/ArrayPerfilDoPersonagem.swift
+++ Julgamento/Model/ArrayPerfilDoPersonagem.swift
@@ -12,15 +12,15 @@ import UIKit
 var arrayPerfilDoPersonagem: [PerfilDoPersonagem] = [
     
 
-    PerfilDoPersonagem(nome: "Unicornio", personalidade: "Ainda vou escrever", papel: "Réu", image: #imageLiteral(resourceName: "image2")),
+    PerfilDoPersonagem(nome: "Unicornio", personalidade: "Unicórnio são conhecidos como um seres muito amigáveis, e por possuir poderes mágicos eles costumam ajudar a outros seres curando ferimentos leves, principalmente das mulheres vila. ", papel: "Réu", image: #imageLiteral(resourceName: "image2")),
     
-    PerfilDoPersonagem(nome: "Elfo", personalidade: "Ainda vou escrever", papel: "Advogado de Dfesa", image: #imageLiteral(resourceName: "image6")),
+    PerfilDoPersonagem(nome: "Elfo", personalidade: "Elfos geralmente são seres belos e muito inteligentes. Tendem a se ofender muito fácil e mudar de humor de uma hora para outra.", papel: "Advogado de Dfesa", image: #imageLiteral(resourceName: "image6")),
     
-    PerfilDoPersonagem(nome: "Gnomo", personalidade: "Ainda vou escrever", papel: "Juri", image: #imageLiteral(resourceName: "image1")),
+    PerfilDoPersonagem(nome: "Gnomo", personalidade: "Gnomos são muito astutos e inteligentes. Tendem a ter e proteger bens preciosos. Alguns são muito bons e gentis embora outros sejam gananciosos e desonestos. ", papel: "Juri", image: #imageLiteral(resourceName: "image1")),
     
-//    PerfilDoPersonagem(nome: "Bruxa", personalidade: "Ainda vou escrever", papel: "Promotora", image: <#T##UIImage#>),
-//
-//    PerfilDoPersonagem(nome: "Diabrete", personalidade: "Ainda vou escrever", papel: "testemunha", image: <#T##UIImage#>),
+    PerfilDoPersonagem(nome: "Bruxa", personalidade: "Bruxas são grandes conhecedoras da natureza e de feitiços mágicos. Algumas costumam utilizar seus poderes e sabedoria para cura, embora outras utilizem para a prática de mágia negra. ", papel: "Promotora", image: <#T##UIImage#>),
+
+    PerfilDoPersonagem(nome: "Diabrete", personalidade: "Diabretes são muito travessos e ajudam as Bruxas e o Senhor das Trevas. São muito pequenos apesar de que alguns dizem que eles podem ter o tamanho que quiserem.", papel: "testemunha", image: <#T##UIImage#>),
 
     
 ]
