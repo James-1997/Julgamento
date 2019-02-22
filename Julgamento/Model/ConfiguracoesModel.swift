89f5f585134d9a4517e965bfd6c57ba9a016fd18
Lucídio Souza <lucidio.abs@gmail.com>
2019-02-21 22:01:41 -0400

    add configuracoesModel e tempo dos jogadores

---
+++ Julgamento/Model/ConfiguracoesModel.swift
@@ -0,0 +1,11 @@
+//
+//  ConfiguracoesModel.swift
+//  Julgamento
+//
+//  Created by Lucídio Andrade Barbosa de Souza on 21/02/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import Foundation
+
+let tempoDosJogadores = [15,90,90,90,60,30,90,90,60,90,90,60,30,30]
