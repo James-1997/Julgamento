b5470bd63a955f6287cf911169d00805bb9283a1
Robson James <james.robson.junior@gmail.com>
2019-02-22 17:56:25 -0400

    tela do número de jogadores Responsividade das interações de incremento e decremento do número de jogadores

--- Julgamento/Controller/SelectPlayersViewController.swift
+++ Julgamento/Controller/SelectPlayersViewController.swift
@@ -10,7 +10,36 @@ import UIKit
 
 class SelectPlayersViewController: UIViewController {
     
+    
+    @IBOutlet weak var playerOneView: UIView!
+    
+    
+    @IBOutlet weak var playerOneLabel: UILabel!
+    
+    @IBOutlet weak var playerTwoView: UIView!
+    
+    @IBOutlet weak var playerTwoLabel: UILabel!
+ 
+    
+    @IBOutlet weak var playerThreeView: UIView!
+    
+    @IBOutlet weak var playerThreeLabel: UILabel!
+    
+    @IBOutlet weak var playerFiveView: UIView!
+    
+    @IBOutlet weak var playerFiveLabel: UILabel!
+    
+   
+    @IBOutlet weak var playerFourView: UIView!
+    
+    @IBOutlet weak var playerFourLabel: UILabel!
+    
+    
     @IBOutlet weak var numDePlayerLabel: UILabel!
+    
+    
+    @IBOutlet weak var playerThreePerfImage: UIImageView!
+    
     
     var quantJogadores = 4
     var juri: Int?
@@ -21,6 +50,8 @@ class SelectPlayersViewController: UIViewController {
             quantJogadores = quantJogadores + 1
             numDePlayerLabel.text = "\(quantJogadores)"
             divPersons()
+            self.playerThreeLabel.text = "1"
+            self.playerThreePerfImage.image = UIImage(named: "gnomo_perfil")
         }
     }
     
@@ -30,9 +61,11 @@ class SelectPlayersViewController: UIViewController {
             numDePlayerLabel.text = "\(quantJogadores)"
             if quantJogadores > 4 {
                 divPersons()
+            } else {
+                self.playerThreeLabel.text = "0"
+                self.playerThreePerfImage.image = UIImage(named: "gnomo_perfil_datived")
             }
         }
-        
       
     }
     
@@ -49,10 +82,11 @@ class SelectPlayersViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        setDesignViews()
         let _StrQtdPlayer = "\(quantJogadores)"
         numDePlayerLabel.text = _StrQtdPlayer
-        
+        self.playerThreePerfImage.image = UIImage(named: "gnomo_perfil_datived")
+        self.playerThreeLabel.text = "0"
     }
    
     
@@ -61,10 +95,18 @@ class SelectPlayersViewController: UIViewController {
         let restDiv = DivForGame(qtdJogadores: qtdJuriTest)
         self.juri = restDiv[0]
         self.test = restDiv[1]
-        print("J - \(juri)")
-        print("T - \(test)")
+        playerFourLabel.text = "\(juri!)"
+        playerFiveLabel.text = "\(test!)"
     }
     
+    func setDesignViews(){
+        self.playerOneView.layer.cornerRadius = playerOneView.frame.size.height / 2
+        self.playerTwoView.layer.cornerRadius = playerTwoView.frame.size.height / 2
+        self.playerThreeView.layer.cornerRadius = playerThreeView.frame.size.height / 2
+        self.playerFourView.layer.cornerRadius = playerFourView.frame.size.height / 2
+        self.playerFiveView.layer.cornerRadius = playerFiveView.frame.size.height / 2
+
+    }
     
 }
 
