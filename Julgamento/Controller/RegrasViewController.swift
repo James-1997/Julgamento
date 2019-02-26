0f5f385d69e2cb009eb90d460c8d63bef016a742
Robson James <james.robson.junior@gmail.com>
2019-02-26 12:00:24 -0400

    TELA DE REGRAS UPDATE

--- Julgamento/Controller/RegrasViewController.swift
+++ Julgamento/Controller/RegrasViewController.swift
@@ -8,14 +8,66 @@
 
 import UIKit
 
-class RegrasViewController: UIViewController {
+class RegrasViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    let menuElements: [String] = ["PAPEIS","RODADAS","VOTAÇÃO","ELEMENTOS ESPECIAIS"]
+    
+    var selectedTableItem: Int = 0
     
     // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuElements.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "tableViewRules", for: indexPath) as! TableViewCellRules
+        
+        cell.titleLabel.text = menuElements[indexPath.row]
+        
+        return cell
+
+    }
+    
+    func tableView(_ tableView: UITableView, didUpdateFocusIn context: UITableViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        
+        if (context.nextFocusedIndexPath == nil) {
+            print("not brwosinf the table")
+            return
+        }
+        
+        //        print(context.nextFocusedIndexPath!.item)
+        
+        selectedTableItem = context.nextFocusedIndexPath!.item
+        
+        print(selectedTableItem)
+        
+        switch menuElements[selectedTableItem]  {
+        case "PAPEIS":
+            print("PAPEIS")
+        case "RODADAS":
+            print("RODADAS")
+        case "VOTAÇÃO" :
+            print("VOTAÇÃO" )
+        case "ELEMENTOS ESPECIAIS" :
+            print("ELEMENTOS ESPECIAIS" )
+        default:
+            return
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
     
     
 }
