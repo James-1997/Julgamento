6da273db1c8999d748019ee17753e09453f8aedf
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-02-27 10:32:33 -0400

    Adiciona Assets da tela "Story"

--- Julgamento/Controller/StoryViewController.swift
+++ Julgamento/Controller/StoryViewController.swift
@@ -13,9 +13,15 @@ class StoryViewController: UIViewController {
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var storyTitle: UILabel!
     @IBOutlet weak var storyText: UILabel!
+    @IBOutlet weak var storyIdLabel: UILabel!
+    
+    
     
     var históriaSelecionada: Story = arrayDeEstórias[0]
     var numJogadores: Int?
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +33,12 @@ class StoryViewController: UIViewController {
  //       storyTitle.text = históriaSelecionada.titulo
         storyText.text = históriaSelecionada.história
         
+        storyIdLabel.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        storyIdLabel.shadowOffset =  CGSize(width: 4, height: 4)
+       storyIdLabel.layer.shadowRadius = 6
+       storyIdLabel.layer.shadowOpacity = -3
+        
+        //515255
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
