218bb8d468a3759167e2dd1b723228423e2c5cb1
Robson James <james.robson.junior@gmail.com>
2019-02-28 18:20:17 -0400

    tirando endTime do jogo: Elemento que servia para o usuário por fim na sua vez antes do fim do jogo

---
+++ Julgamento/Controller/New Group/buttonFocus.swift
@@ -0,0 +1,21 @@
+//
+//  buttonFocus.swift
+//  Julgamento
+//
+//  Created by Robson James Junior on 28/02/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import UIKit
+
+class buttonFocus: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
