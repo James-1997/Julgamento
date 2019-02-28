d15acf8efd735662ebb0f77a2e0aa0b0963bad2a
Lucídio Souza <lucidio.abs@gmail.com>
2019-02-28 09:01:12 -0400

    add sounds file

---
+++ Julgamento/Controller/Sounds/PowerUps.swift
@@ -0,0 +1,9 @@
+//
+//  PowerUps.swift
+//  Julgamento
+//
+//  Created by Lucídio Andrade Barbosa de Souza on 28/02/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import Foundation
---
+++ Julgamento/Controller/Sounds/Turnos.swift
@@ -0,0 +1,9 @@
+//
+//  Turnos.swift
+//  Julgamento
+//
+//  Created by Lucídio Andrade Barbosa de Souza on 28/02/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import Foundation
